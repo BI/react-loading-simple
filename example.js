baee7161a68350b4acdfe4db402f5b181cf55c70
Stephen Smith <stephensmith@Stephens-MacBook-Pro.local>
2016-05-23 18:03:40 -0400

    updated to React 15

--- example.js
+++ example.js
@@ -1,5 +1,6 @@
-var React = require('react/addons');
-var ReactLoadingSimple = require('./lib/index.js');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactLoadingSimple = require('./src/react-loading-simple.js');
 
 var DemoLoading = React.createClass({
 
@@ -10,17 +11,17 @@ var DemoLoading = React.createClass({
 	render: function() {
 		return (
 			<div>
-				<ReactLoadingSimple loading={this.state.loading} type="balls" />
-				<ReactLoadingSimple loading={this.state.loading} type="bars"/>
-				<ReactLoadingSimple loading={this.state.loading} type="blank"/>
-				<ReactLoadingSimple loading={this.state.loading} type="bubbles"/>
-				<ReactLoadingSimple loading={this.state.loading} type="cubes"/>
-				<ReactLoadingSimple loading={this.state.loading} type="cylon"/>
-				<ReactLoadingSimple loading={this.state.loading} type="spin"/>
-				<ReactLoadingSimple loading={this.state.loading} type="spinningBubbles"/>
-				<ReactLoadingSimple loading={this.state.loading} type="spokes"/>
+				<ReactLoadingSimple loading={this.state.loading} type='balls' />
+				<ReactLoadingSimple loading={this.state.loading} type='bars'/>
+				<ReactLoadingSimple loading={this.state.loading} type='blank'/>
+				<ReactLoadingSimple loading={this.state.loading} type='bubbles'/>
+				<ReactLoadingSimple loading={this.state.loading} type='cubes'/>
+				<ReactLoadingSimple loading={this.state.loading} type='cylon'/>
+				<ReactLoadingSimple loading={this.state.loading} type='spin'/>
+				<ReactLoadingSimple loading={this.state.loading} type='spinningBubbles'/>
+				<ReactLoadingSimple loading={this.state.loading} type='spokes'/>
 			</div>
 		);
 	}
 });
-React.render(React.createElement(DemoLoading), document.getElementById('main'));
+ReactDOM.render(React.createElement(DemoLoading), document.getElementById('main'));
